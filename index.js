5201761db870ee1228485ba112a3fd3050656ce8
Kuria300 <eugenekuria66@gmail.com>
2025-12-15 15:15:59 +0300

    changes

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+const datejs= require('datejs')
+
+function combineUsers(...args){
+  let combinedObject={
+    users: [],
+    merge_date: new Date().toString("M/d/yyyy")
+  }
+//  let arr= [...args].flat()
+//  arr.map((val)=> combinedObject.users.push(val))
+  args.map((eachArr)=>(
+     combinedObject.users.push(...eachArr)
+  ))
+ 
+  return combinedObject
+}
 
 
+//combineUsers(["Jim3","Pam5","Dwight77"],["Michael6","Eleanor22","Chidi202"],["Jack_jack","Julia_Oreo", "Bill_bore"])
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
